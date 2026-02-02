b6009069b3cd2e012a7af7034b3bc3b9d6912fb1
IT25100626 <it25100626@my.sliit.lk>
2026-02-02 18:25:23 +0530

    programming tutorial - question 5

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        int answer1 = square(add(multiply(3,4),multiply(5,7)));
+        System.out.println(answer1);
+
+        int answer2 = add(square(add(4,7)),square(add(8,3)));
+        System.out.println(answer2);
+    }
+
+    public static int add (int a , int b ){return a+b;}
+    public static int multiply (int a , int b){return a*b;}
+    public static int square (int a){return a*a;}
+}
